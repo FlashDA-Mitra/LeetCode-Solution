152d22eb6accec5a0b069a2709049fd1986b3ec5
Anish Mitra <110296367+FlashDA-Mitra@users.noreply.github.com>
2026-08-03 00:11:20 +0530

    Time: 0 ms (100%), Space: 10 MB (47.08%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target) {
+    int i = 0;
+    int j = numsSize - 1;
+    int mid;
+
+    while ( i <= j){
+        mid = (i + j)/2;
+        if(nums[mid] == target){
+            return mid;
+        } else if(nums[mid] > target){
+            j = mid - 1;
+        } else{
+            i = mid + 1;
+        }
+    }
+
+    return -1;
+}
