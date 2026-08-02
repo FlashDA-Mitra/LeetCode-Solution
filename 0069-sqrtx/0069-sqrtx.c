5610a8294912e80896d5db0c5eaa13d0cf7de384
Anish Mitra <110296367+FlashDA-Mitra@users.noreply.github.com>
2026-08-02 23:10:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (12.65%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,26 @@
+int mySqrt(int x) {
+    if(x == 0 || x == 1){
+        return x;
+    }
+    int low = 1;
+    int high = x/2;
+    int mid;
+    int ans = 0;
+
+    while (low <= high)
+    {
+        mid = (low + high)/2;
+        
+        if (mid == x/mid){
+            return mid;
+        } 
+        else if(mid < x/mid){
+            ans = mid;
+            low = mid + 1;
+        } else{
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
